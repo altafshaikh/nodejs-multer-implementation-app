e575ad2b6a56251bffc8d0515b98135945b2eb35
altafshaikh <iamaltafshaikh07@gmail.com>
2020-12-16 16:58:44 +0530

    multer implemented for the express app

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
+var multer = require("multer");
+
 const PORT = 3000;
+
+//multer
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    var directory = path.join(__dirname, "uploads");
+    if (!fs.existsSync(directory)) {
+      fs.mkdirSync(directory);
+    }
+    cb(null, directory);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+var upload = multer({ storage: storage });
 
 const app = express();
 
 app.get("/", (req, res) => {
-  console.log("REquest recieved");
   res.send("<h1>Message from server</h1>");
+});
+
+app.post("/profile", upload.single("avatar"), function (req, res, next) {
+  res.status(200);
+  res.json({ message: "file uploaded sucessfully" });
 });
 
 app.listen(PORT, () => {
